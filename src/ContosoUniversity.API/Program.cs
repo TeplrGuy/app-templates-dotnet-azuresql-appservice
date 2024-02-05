1e0676c958bc067b3e9a55ccaec0d597b1a6d736
Gibs <gappiah@DESKTOP-9OAUMU5>
2024-02-05 13:06:54 -0500

    update

--- src/ContosoUniversity.API/Program.cs
+++ src/ContosoUniversity.API/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-
+using Microsoft.ApplicationInsights.DependencyCollector;
 
 
 
