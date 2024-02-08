940c7011af8953737f9f7fa2e6b4f1fec0a7b5c7
Gibs <gilbertappiah@icloud.com>
2024-02-08 12:31:45 -0500

    Update Index.cshtml.cs
    Make SNAT issue obvious

--- src/ContosoUniversity.WebApplication/Pages/Departments/Index.cshtml.cs
+++ src/ContosoUniversity.WebApplication/Pages/Departments/Index.cshtml.cs
@@ -11,6 +11,7 @@ namespace ContosoUniversity.WebApplication.Pages.Departments
     public class IndexModel : PageModel
     {
         private readonly IHttpClientFactory client; 
+        private static readonly HttpClient httpClient = new HttpClient(); 
         string URLAPI = Environment.GetEnvironmentVariable("URLAPI");
        
 
@@ -21,15 +22,17 @@ namespace ContosoUniversity.WebApplication.Pages.Departments
 
         public Models.APIViewModels.DepartmentResult Department { get; set; }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
             var request = HttpWebRequest.Create(URLAPI + "/api/Departments");
-            var response = request.GetResponse();
+            var response = await request.GetResponseAsync();
             using (var streamReader = new StreamReader(response.GetResponseStream()))
             {
-                var responseContent = streamReader.Read();
-                Department = JsonConvert.DeserializeObject<Models.APIViewModels.DepartmentResult>(responseContent.ToString());
+                var responseContent = await streamReader.ReadToEndAsync();
+                Department = JsonConvert.DeserializeObject<Models.APIViewModels.DepartmentResult>(responseContent);
             }
+            var causeDelay = HttpWebRequest.Create("https://app.requestly.io/delay/5000/https://app.requestly.io/echo");
+            causeDelay.GetResponse();
         }
     }
 }
