8255634e8684dcf51f79fa2de0a049a0d82527a5
Gibs <gilbertappiah@icloud.com>
2024-02-08 18:21:05 -0500

    Update Program.cs

--- src/ContosoUniversity.API/Program.cs
+++ src/ContosoUniversity.API/Program.cs
@@ -1,59 +1,51 @@
-﻿using Azure.Identity;
-using System;
-using ContosoUniversity.API.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.ApplicationInsights.DependencyCollector;
-using Microsoft.ApplicationInsights.Extensibility;
-
-
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<ITelemetryInitializer, MyTelemetryInitializer>();
-var connectionString = "";
-if (builder.Configuration["AZURE_KEY_VAULT_ENDPOINT"] != null)
+builder.Services.Configure<CookiePolicyOptions>(options =>
 {
-    var credential = new DefaultAzureCredential();
-    builder.Configuration.AddAzureKeyVault(new Uri(builder.Configuration["AZURE_KEY_VAULT_ENDPOINT"]), credential);
-    connectionString = builder.Configuration[builder.Configuration["AZURE_SQL_CONNECTION_STRING_KEY"]];
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+// this environmet is for docker
+if (builder.Configuration["URLAPI"] != null)
+{
+    builder.Services.AddHttpClient("client", client => { client.BaseAddress = new Uri(builder.Configuration["URLAPI"]); });
 }
 else
 {
-    connectionString = builder.Configuration.GetConnectionString("ContosoUniversityAPIContext");
+    var section = builder.Configuration.GetSection("Api");
+    builder.Services.AddHttpClient("client", client => { client.BaseAddress = new Uri(section["Address"]); });
 }
 
-builder.Services.AddDbContext<ContosoUniversityAPIContext>(options =>
+builder.Services.AddRazorPages();
+if (builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"] != null)
 {
-    options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
-});
-
-builder.Services.AddControllers();
-builder.Services.AddApplicationInsightsTelemetry(builder.Configuration);
-builder.Services.AddServiceProfiler(builder.Configuration);
-
-builder.Services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, options) =>
+    builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
+} else
 {
-    module.EnableSqlCommandTextInstrumentation = true;
-});
+    builder.Services.AddApplicationInsightsTelemetry();
+}
 
 var app = builder.Build();
-
-await using (var scope = app.Services.CreateAsyncScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ContosoUniversityAPIContext>();
-    await DbInitializer.Initialize(db);
-}
-
 
 //if (env.IsDevelopment())
 //{
 //    app.UseDeveloperExceptionPage();
 //}
 
+app.UseExceptionHandler("/Error");
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
+
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
@@ -61,11 +53,7 @@ app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllers();
+    endpoints.MapRazorPages();
 });
-
-// Register the Swagger generator and the Swagger UI middlewares
-app.UseOpenApi();
-app.UseSwaggerUi3();
 
 app.Run();
