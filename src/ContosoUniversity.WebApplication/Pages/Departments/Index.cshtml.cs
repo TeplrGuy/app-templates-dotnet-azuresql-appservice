34813bd56d0b9af051c8a44065f8ff755698ab28
Gibs <gilbertappiah@icloud.com>
2024-02-09 20:41:32 -0500

    Update Index.cshtml.cs

--- src/ContosoUniversity.WebApplication/Pages/Departments/Index.cshtml.cs
+++ src/ContosoUniversity.WebApplication/Pages/Departments/Index.cshtml.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -10,10 +7,7 @@ namespace ContosoUniversity.WebApplication.Pages.Departments
 {
     public class IndexModel : PageModel
     {
-        private readonly IHttpClientFactory client; 
-        private static readonly HttpClient httpClient = new HttpClient(); 
-        string URLAPI = Environment.GetEnvironmentVariable("URLAPI");
-       
+        private readonly IHttpClientFactory client;
 
         public IndexModel(IHttpClientFactory client)
         {
@@ -24,15 +18,8 @@ namespace ContosoUniversity.WebApplication.Pages.Departments
 
         public async Task OnGetAsync()
         {
-            var request = HttpWebRequest.Create(URLAPI + "/api/Departments");
-            var response = await request.GetResponseAsync();
-            using (var streamReader = new StreamReader(response.GetResponseStream()))
-            {
-                var responseContent = await streamReader.ReadToEndAsync();
-                Department = JsonConvert.DeserializeObject<Models.APIViewModels.DepartmentResult>(responseContent);
-            }
-            var causeDelay = HttpWebRequest.Create("https://app.requestly.io/delay/5000/https://app.requestly.io/echo");
-            causeDelay.GetResponse();
+            var response = await client.CreateClient("client").GetStringAsync("api/Departments");
+            Department = JsonConvert.DeserializeObject<Models.APIViewModels.DepartmentResult>(response);
         }
     }
 }
